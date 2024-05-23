db4f1517911773daab849b95e7ca3c6f68e69cfb
Julian19G <Julian19guerra@gmail.com>
2024-05-23 13:24:46 -0500

    arreglos

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/clientes">Cliente</router-link> |
+    <router-link to="/productos">Productos</router-link> |
+    <router-link to="/metodos">Metodo Pago</router-link> |
+    <router-link to="/categorias">Categorias</router-link>
   </nav>
   <router-view/>
 </template>
